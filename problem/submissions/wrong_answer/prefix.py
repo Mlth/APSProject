37e9edb888632e3d954c2c5e6a1b3db5b4128ad2
Mlth <mlth@itu.dk>
2023-04-30 15:48:30 +0200

    Finish prefix solution. Change test 1 to have updates before gets only. Add folders for fenwick wrong_answer solution

--- problem/submissions/wrong_answer/prefix.py
+++ problem/submissions/wrong_answer/prefix.py
@@ -7,7 +7,7 @@ array = []
 array.extend(map(int, input().split()))
 
 calculations = defaultdict(int)
-powerOfTwos = [0]
+powerOfTwos = []
 
 minMode = False
 for i in range(O):
@@ -16,15 +16,19 @@ for i in range(O):
     fst, snd = map(int, [operation[1], operation[2]])
     if(operationType == "update"):
         array[fst-1] = snd
+
     elif(operationType == "min" and not minMode):
         for i in range(1, N+1):
             log2Calc = math.log2(i)
             if(math.ceil(log2Calc) == math.floor(log2Calc)):
-                powerOfTwos.append(i)
+                powerOfTwos.append(i - 1)
 
         for i in powerOfTwos:
             for a in range(N - i):
-                calculations[(a, a+i)] = min(array[a], array[a+i])
+                calculations[(a, a+i)] = min((array[gi] for gi in range(a, a+i)), default=array[a])
         minMode = True
-    elif(operationType == "min"):
-        break
+        
+    if(operationType == "min"):
+        largestPowerOfTwo = 2**((snd - fst + 1).bit_length() - 1)
+        minValue = min(calculations[fst - 1, fst + largestPowerOfTwo - 1 - 1], calculations[snd - 1 - largestPowerOfTwo + 1, snd - 1])
+        print(minValue)
